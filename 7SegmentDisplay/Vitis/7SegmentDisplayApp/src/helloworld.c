e905caaba21c0eeae24a89335312ca43d14bc97e
Philippe-Arnaud Hiroux <12002903@student.pxl.be>
2023-05-31 15:11:26 +0200

    Working vitis code

--- 7SegmentDisplay/Vitis/7SegmentDisplayApp/src/helloworld.c
+++ 7SegmentDisplay/Vitis/7SegmentDisplayApp/src/helloworld.c
@@ -1,61 +1,47 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
+#include "xil_printf.h"
+#include "xgpio.h"
+#include "xparameters.h"
+#include "sleep.h"
 
+#define SIZE 	10
+					 //ABCD .EFG
+#define ONE 	0x60 //0110 0000
+#define TWO		0xD5 //1101 0101
+#define THREE	0xF1 //1111 0001
+#define FOUR	0x63 //0110 0011
+#define FIVE	0xB3 //1011 0011
+#define SIX		0xB7 //1011 0111
+#define SEVEN	0xE0 //1110 0000
+#define EIGHT	0xF7 //1111 0111
+#define NINE	0xF3 //1111 0011
+#define DOT		0x8	 //0000 1000
+
+
+XGpio Gpio;
 
 int main()
 {
     init_platform();
 
-    print("Hello World\n\r");
-    print("Successfully ran Hello World application");
+    char number[SIZE] = {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, DOT};
+
+    XGpio_Config *GpioConfig = XGpio_LookupConfig(XPAR_AXI_GPIO_0_DEVICE_ID);
+	XGpio_CfgInitialize(&Gpio, GpioConfig, GpioConfig->BaseAddress);
+	XGpio_Initialize(&Gpio, XPAR_AXI_GPIO_0_DEVICE_ID);
+	XGpio_SetDataDirection(&Gpio, 1, 0);
+
+	while(1)
+	{
+		for(int i=0; i<SIZE; i++)
+		{
+			XGpio_DiscreteWrite(&Gpio, 1, number[i]);
+			sleep(1);
+		}
+	}
+
     cleanup_platform();
     return 0;
 }
