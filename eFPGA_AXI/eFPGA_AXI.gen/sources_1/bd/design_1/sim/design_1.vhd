1fd44a30e0c1a55c6b82a7c15dbff1869e42b8d0
Philippe-Arnaud Hiroux <12002903@student.pxl.be>
2023-06-03 15:58:47 +0200

    synthesis and implemantaion

--- eFPGA_AXI/eFPGA_AXI.gen/sources_1/bd/design_1/sim/design_1.vhd
+++ eFPGA_AXI/eFPGA_AXI.gen/sources_1/bd/design_1/sim/design_1.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
---Date        : Sat Jun  3 15:46:25 2023
+--Date        : Sat Jun  3 15:47:15 2023
 --Host        : DESKTOP-89VC88I running 64-bit major release  (build 9200)
 --Command     : generate_target design_1.bd
 --Design      : design_1
